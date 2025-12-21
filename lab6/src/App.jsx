2b8654c93e5a76a5875b2fae27098804f38b1ce3
Hadil <ichtahadil56@gmail.com>
2025-12-21 19:09:36 +0000

    Add lab6: vite/react project

---
+++ lab6/src/App.jsx
@@ -0,0 +1,24 @@
+import ProfileCard from './components/ProfileCard';
+
+function App() {
+  return (
+    <div>
+      <h1>My Team</h1>
+
+      {/* Utilisation du composant avec différents props */}
+      <ProfileCard
+        name="Adil Chekati"
+        role="Senior Lecturer"
+        email="adil.chekati@univ-constantine2.dz"
+      />
+
+      <ProfileCard
+        name="Mohamed Amin Bouchehed"
+        role="Frontend Engineer"
+        email="aminlazir@gmail.com"
+      />
+    </div>
+  );
+}
+
+export default App;
